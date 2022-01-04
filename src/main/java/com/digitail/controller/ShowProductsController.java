cffe402a685edbc2c12c08846209412bc33dc783
Daniil <75034877+DaniilVhivtsev@users.noreply.github.com>
2022-01-04 20:45:17 +0500

    Сделал скачивание zip архива

--- src/main/java/com/digitail/controller/ShowProductsController.java
+++ src/main/java/com/digitail/controller/ShowProductsController.java
@@ -7,6 +7,7 @@ import com.digitail.model.Status;
 import com.digitail.model.User;
 import com.digitail.repos.ProductRepo;
 import com.digitail.repos.UserRepo;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.InputStreamResource;
@@ -20,13 +21,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import javax.validation.Valid;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @Controller
 @RequestMapping("/product")
@@ -86,7 +91,7 @@ public class ShowProductsController {
     public ResponseEntity<Object> downloadFile(@PathVariable("id") long id, @AuthenticationPrincipal User user) throws FileNotFoundException {
         var product = productRepo.findById(id);
         String fileName = product.getPath();
-        var file = new File("src/main/resources/static/" + fileName);
+        var file = new File("uploads/" + fileName);
         var resources = new InputStreamResource(new FileInputStream(file));
         var headers = new HttpHeaders();
         headers.add("Content-Disposition",
@@ -99,5 +104,30 @@ public class ShowProductsController {
                 .contentLength(file.length())
                 .contentType(MediaType.IMAGE_PNG).body(resources);
         return responseEntity;
+    }
+
+    @GetMapping("/download")
+    public ResponseEntity<StreamingResponseBody> downloadProducts(@AuthenticationPrincipal User user) throws FileNotFoundException {
+        return ResponseEntity.ok()
+                .header("Content-Disposition", "attachment; filename=\"test.zip\"")
+                .body(out -> {
+                    var zipOutputStream = new ZipOutputStream(out);
+
+                    // package files
+                    for (var product : productRepo.findAll()) {
+                        var file = new File("uploads/" + product.getPath());
+
+                        //new zip entry and copying inputstream with file to zipOutputStream, after all closing streams
+                        zipOutputStream.putNextEntry(new ZipEntry(file.getName()));
+                        FileInputStream fileInputStream = new FileInputStream(file);
+
+                        IOUtils.copy(fileInputStream, zipOutputStream);
+
+                        fileInputStream.close();
+                        zipOutputStream.closeEntry();
+                    }
+
+                    zipOutputStream.close();
+                });
     }
 }
